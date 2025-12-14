a99db0810ebcc60c7ebd957ac45824f677fb782a
Ralexalex <x21331013@student.ncirl.ie>
2025-12-14 08:05:18 +0000

    Implemented password hashing for more secure auth

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "dev-secret-key"
@@ -18,11 +19,14 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        hashed_password = generate_password_hash(password)
 
         conn = get_db_connection()
         conn.execute(
-            f"INSERT INTO users (username, password, role) VALUES ('{username}', '{password}', 'user')"
+            "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+            (username, hashed_password, "user")
         )
+
         conn.commit()
         conn.close()
 
@@ -45,13 +49,12 @@ def login():
 
         conn = get_db_connection()
         user = conn.execute(
-        "SELECT * FROM users WHERE username=? AND password=?",
-        (username, password)
-    ).fetchone()
-
+            "SELECT * FROM users WHERE username=?",
+            (username,)
+        ).fetchone()
         conn.close()
 
-        if user:
+        if user and check_password_hash(user["password"], password):
             session["user_id"] = user["id"]
             session["role"] = user["role"]
             return redirect("/dashboard")
@@ -66,6 +69,7 @@ def login():
             <button type="submit">Login</button>
         </form>
     """
+
 
 @app.route("/dashboard")
 def dashboard():
